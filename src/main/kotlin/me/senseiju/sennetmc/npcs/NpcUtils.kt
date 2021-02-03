89ae9ea350668037122d21c3f31f1894ed680ac7
SenseiJu <chathataranjit@yahoo.co.uk>
2021-02-03 14:21:28 +0000

    Added growth-rate to upgrades file

--- src/main/kotlin/me/senseiju/sennetmc/npcs/NpcUtils.kt
+++ src/main/kotlin/me/senseiju/sennetmc/npcs/NpcUtils.kt
@@ -1,13 +1,17 @@
 package me.senseiju.sennetmc.npcs
 
+import me.senseiju.sennetmc.SennetMC
 import me.senseiju.sennetmc.npcs.types.NpcType
 import net.citizensnpcs.api.CitizensAPI
 import net.citizensnpcs.api.npc.NPC
 import net.citizensnpcs.trait.LookClose
 import org.bukkit.entity.EntityType
+import org.bukkit.plugin.java.JavaPlugin
 import kotlin.math.pow
 
 private val REGISTRY = CitizensAPI.getNPCRegistry()
+private val plugin = JavaPlugin.getPlugin(SennetMC::class.java)
+private val upgradesFile = plugin.upgradesManager.upgradesFile
 
 fun createBasicNpc(npcType: NpcType) : NPC {
     val npc = REGISTRY.createNPC(EntityType.PLAYER, npcType.npcName)
@@ -19,6 +23,7 @@ fun createBasicNpc(npcType: NpcType) : NPC {
     return npc
 }
 
-fun calculateNextUpgradeCost(baseCost: Double, currentUpgrades: Int, growthRate: Double = 1.15) : Double {
+fun calculateNextUpgradeCost(baseCost: Double, currentUpgrades: Int,
+                             growthRate: Double = upgradesFile.config.getDouble("growth-rate", 1.3)) : Double {
     return "%.2f".format(baseCost * growthRate.pow(currentUpgrades)).toDouble()
 }
